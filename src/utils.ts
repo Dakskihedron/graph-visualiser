a4da6c8df8f41c9fab6a79fbf2b8ce04e2ef0695
Dakskihedron <47210659+Dakskihedron@users.noreply.github.com>
2022-06-07 23:59:00 +1200

    Add file for util funcs and classes

---
+++ src/utils.ts
@@ -0,0 +1,33 @@
+export async function wait(ms: number = 1000): Promise<void> {
+    return new Promise<void>((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+export class Queue<T> {
+    private _data: T[] = new Array();
+
+    public enqueue(item: T): void {
+        this._data.push(item);
+    }
+
+    public dequeue(): void | undefined {
+        this._data.splice(0, 1);
+    }
+
+    public peek(): T | undefined {
+        return this._data[0];
+    }
+
+    public size(): number {
+        return this._data.length;
+    }
+
+    public isEmpty(): boolean {
+        return this._data.length == 0;
+    }
+
+    public toString(): string {
+        return `<- |${this._data.join(", ")}| <-`
+    }
+}
