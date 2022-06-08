91b40b3c1a3cc7b279afc8c9c6b2bb7226478521
Dakskihedron <47210659+Dakskihedron@users.noreply.github.com>
2022-06-09 01:23:43 +1200

    Improve variable and func naming

--- src/index.ts
+++ src/index.ts
@@ -1,193 +1,204 @@
-import BFS from './algorithms/bfs.js';
-import DFS from './algorithms/dfs.js';
-import GraphNode from './graph-node.js';
+import BFS from "./algorithms/bfs.js";
+import DFS from "./algorithms/dfs.js";
+import Vertex from "./vertex.js";
 
 enum EditorMode {
     AddElement = "addElement",
-    RemoveElement = "removeElement"
+    RemoveElement = "removeElement",
 }
 
 enum AlgorithmSelection {
     BFS = "bfs",
-    DFS = "dfs"
+    DFS = "dfs",
 }
 
 abstract class GraphEditor {
-    private static _graphNodesList: GraphNode[] = new Array();
-    private static _currentNodeValues: number[] = new Array();
-    private static _editorSelectedMode: EditorMode = EditorMode.AddElement;
-    private static readonly _graphEditorWindow: SVGGElement = document.getElementById("graphEditorWindow") as unknown as SVGGElement;
+    private static readonly graphEditor: SVGSVGElement = document.getElementById(
+        "editorWindow"
+    ) as unknown as SVGSVGElement;
 
-    private static _runningProcess: boolean = false;
-    private static _startNode: GraphNode | null = null;
-    private static _awaitingEndNode: boolean = false;
+    private static vertices: Vertex[] = new Array();
+    private static reservedValues: number[] = new Array();
+    private static selectedEditorMode: EditorMode = EditorMode.AddElement;
 
-    public static getNextNodeValue(): number | null {
-        for (let n=0; n < 100; n++) {
-            if (this._currentNodeValues.indexOf(n) == -1) return n;
+    private static inProgress: boolean = false;
+    private static selectedVertex: Vertex | null = null;
+    private static drawingEdge: boolean = false;
+
+    public static getNextValue(): number | undefined {
+        for (let n = 0; n < 100; n++) {
+            if (this.reservedValues.indexOf(n) == -1) return n;
         }
-        return null;
     }
 
-    public static convertMousePosToSVGPos(event: MouseEvent): { x: number, y: number } {
-        const ctm = (this._graphEditorWindow).getScreenCTM();
+    public static translatePos(event: MouseEvent): { x: number; y: number } {
+        const ctm = this.graphEditor.getScreenCTM();
         return { x: (event.clientX - ctm!.e) / ctm!.a, y: (event.clientY - ctm!.f) / ctm!.d };
     }
 
-    public static async addNodeToGraph(x: number, y: number): Promise<boolean> {
-        const nodeValue = this.getNextNodeValue();
-        if (nodeValue == null) return false; 
+    public static getVertex(id: string): Vertex {
+        const vertexId = parseInt(id);
+        return this.vertices.find(v => v.getVertexValue() == vertexId)!;
+    }
 
-        const newGraphNode = new GraphNode(nodeValue, x, y);
-        this._graphNodesList.push(newGraphNode);
-        this._currentNodeValues.push(nodeValue);
+    public static async addVertex(x: number, y: number): Promise<boolean> {
+        const value = this.getNextValue();
+        if (value == undefined) return false;
+
+        const newVertex = new Vertex(value, x, y);
+        newVertex.createDOMObject(x, y);
+        this.vertices.push(newVertex);
+        this.reservedValues.push(value);
         return true;
     }
-    
-    public static async removeNodeFromGraph(node: HTMLElement): Promise<void> {
-        const index = this._graphNodesList.map(node => node.getNodeValue()).indexOf(parseInt(node.id));
-        const nodeObj = this._graphNodesList[index];
 
-        nodeObj.getListOfEdges().slice().reverse().forEach(edge => this.removeEdgeFromGraph(edge));
-        this._graphNodesList.filter(node => node.hasAdjacentNode(nodeObj)).forEach(node => node.removeAdjacentNode(nodeObj));
-
-        nodeObj.destoryDOMElement();
-        this._graphNodesList.splice(index, 1);
-        this._currentNodeValues.splice(this._currentNodeValues.indexOf(parseInt(node.id)), 1);
+    public static async removeVertex(element: SVGGElement): Promise<void> {
+        const vertex = this.getVertex(element.id);
+        vertex
+            .getEdges()
+            .slice()
+            .reverse()
+            .forEach(edge => this.removeEdge(edge));
+        this.vertices.filter(v => v.hasNeighbour(vertex)).forEach(v => v.removeNeighbour(vertex));
+        vertex.destroyDOMObject();
+        this.vertices.splice(this.vertices.indexOf(vertex), 1);
+        this.reservedValues.splice(this.reservedValues.indexOf(parseInt(element.id)), 1);
     }
 
-    public static async startAddEdgeToGraph(node: SVGGElement): Promise<void> {
-        this._startNode = this._graphNodesList[this._graphNodesList.map(node => node.getNodeValue()).indexOf(parseInt(node.id))];
-        this._awaitingEndNode = true;
-        this._startNode.toggleSelected();
+    public static async addEdgeStart(element: SVGGElement): Promise<void> {
+        this.selectedVertex = this.getVertex(element.id);
+        this.drawingEdge = true;
+        this.selectedVertex.toggleSelected();
     }
 
-    public static async finishAddEdgeToGraph(node: SVGGElement): Promise<void> {
-        const nodeObj = this._graphNodesList[this._graphNodesList.map(node => node.getNodeValue()).indexOf(parseInt(node.id))];
-        if (nodeObj == this._startNode) return;
+    public static async addEdgeEnd(element: SVGGElement): Promise<void> {
+        const vertex = this.getVertex(element.id);
+        if (vertex == this.selectedVertex || this.selectedVertex!.hasNeighbour(vertex)) return;
 
-        if (this._startNode!.hasAdjacentNode(nodeObj)) return;
-        this._startNode!.addAdjacentNode(nodeObj);
+        this.selectedVertex!.addNeighbour(vertex);
 
         const newEdge = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        const startCoord = this._startNode!.getNodeCoordinates();
-        const endCoord = nodeObj.getNodeCoordinates();
-        newEdge.setAttributeNS(null, "d", `M ${startCoord.x},${startCoord.y} L ${endCoord.x},${endCoord.y}`);
-        newEdge.setAttributeNS(null, "stroke", "black");
-        newEdge.setAttributeNS(null, "stroke-width", "3");
-        newEdge.setAttributeNS(null, "fill", "none");
-        newEdge.setAttributeNS(null, "marker-end", "url(#arrow)");
-        
-        this._startNode!.addEdge(newEdge);
-        nodeObj.addEdge(newEdge);
-        document.getElementById("graphEditorEdgesGroup")!.appendChild(newEdge);
+        const startCoord = this.selectedVertex!.getVertexCoords();
+        const endCoord = vertex.getVertexCoords();
+
+        const edgeAttributes: { [key: string]: string } = {
+            class: "edge",
+            d: `M ${startCoord.x},${startCoord.y} L ${endCoord.x},${endCoord.y}`,
+            stroke: "black",
+            "stroke-width": "3",
+            fill: "none",
+            "marker-end": "url(#arrow)",
+        };
+
+        Object.keys(edgeAttributes).forEach(attr => {
+            newEdge.setAttributeNS(null, attr, edgeAttributes[attr]);
+        });
+
+        this.selectedVertex!.addEdge(newEdge);
+        vertex.addEdge(newEdge);
+        document.getElementById("graphEdgesGroup")!.appendChild(newEdge);
     }
 
-    public static async removeEdgeFromGraph(edge: SVGPathElement): Promise<void> {
-        for (const node1 of this._graphNodesList.filter(node => node.hasEdge(edge))) {
-            for (const node2 of this._graphNodesList.filter(node => node.hasEdge(edge))) {
-                if (node1.hasAdjacentNode(node2)) node1.removeAdjacentNode(node2);
+    public static async removeEdge(edge: SVGPathElement): Promise<void> {
+        for (const v1 of this.vertices.filter(v => v.hasEdge(edge))) {
+            for (const v2 of this.vertices.filter(v => v.hasEdge(edge))) {
+                if (v1.hasNeighbour(v2)) v1.hasNeighbour(v2);
             }
         }
-        this._graphNodesList.filter(node => node.hasEdge(edge)).forEach(node => node.removeEdge(edge));
+        this.vertices.filter(v => v.hasEdge(edge)).forEach(v => v.removeEdge(edge));
         edge.remove();
     }
 
-    public static async addElementToGraph(event: MouseEvent): Promise<void> {
-        if (this._awaitingEndNode == true) {
-            if (this._editorSelectedMode == EditorMode.AddElement) {
-                const element = (event.target as HTMLElement).parentNode;
-                if ((element instanceof SVGGElement) && (element.classList.contains("graph-node"))) {
-                    await this.finishAddEdgeToGraph(element);
+    public static async editorAddElement(event: MouseEvent): Promise<void> {
+        const element = (event.target as Element).parentElement;
+        if (this.drawingEdge == true) {
+            if (this.selectedEditorMode == EditorMode.AddElement) {
+                if (element instanceof SVGGElement && element.classList.contains("vertex")) {
+                    await this.addEdgeEnd(element);
                 }
             }
-            this._startNode!.toggleSelected();
-            this._startNode = null;
-            this._awaitingEndNode = false;
-        } else if (event.target == this._graphEditorWindow) {
-            const coord = this.convertMousePosToSVGPos(event);
-            const success = await this.addNodeToGraph(coord.x, coord.y);
-            if (success == false) {
-                alert("Reached node limit.");
-            }
-        } else {
-            const element = (event.target as HTMLElement).parentNode;
-            if ((element instanceof SVGGElement) && (element.classList.contains("graph-node"))) {
-                await this.startAddEdgeToGraph(element);
-            }
+            this.selectedVertex!.toggleSelected();
+            this.selectedVertex = null;
+            this.drawingEdge = false;
+        } else if (element instanceof SVGGElement && element.classList.contains("vertex")) {
+            await this.addEdgeStart(element);
+        } else if (event.target == this.graphEditor) {
+            const coord = this.translatePos(event);
+            const success = await this.addVertex(coord.x, coord.y);
+            if (success == false) alert("Vertex limit reached.");
         }
     }
 
-    public static async removeElementFromGraph(event: MouseEvent): Promise<void> {
-        const selectedElement: HTMLElement = (event.target as HTMLElement).parentElement as HTMLElement;
-
-        if (selectedElement.classList.contains("graph-node")) {
-            this.removeNodeFromGraph(selectedElement);
-        } else if (event.target instanceof SVGPathElement) {
-            this.removeEdgeFromGraph(event.target);
+    public static async editorRemoveElement(event: MouseEvent): Promise<void> {
+        const element = event.target as Element;
+        if (element instanceof SVGPathElement) {
+            await this.removeEdge(element as SVGPathElement);
+        } else if (element.parentElement instanceof SVGGElement && element.parentElement.classList.contains("vertex")) {
+            await this.removeVertex(element.parentElement as SVGGElement);
         }
     }
 
-    public static async editorInteractionEvent(event: MouseEvent): Promise<void> {
-        if (this._runningProcess == true) return;
-        this._runningProcess = true;
+    public static async editorEvent(event: MouseEvent): Promise<void> {
+        if (this.inProgress == true) return;
+        this.inProgress = true;
 
-        if (this._awaitingEndNode == true) {
-            await this.addElementToGraph(event);
-            this._runningProcess = false;
+        if (this.drawingEdge == true) {
+            await this.editorAddElement(event);
+            this.inProgress = false;
             return;
         }
 
-        switch (this._editorSelectedMode) {
+        switch (this.selectedEditorMode) {
             case EditorMode.AddElement:
-                    await this.addElementToGraph(event);
+                await this.editorAddElement(event);
                 break;
             case EditorMode.RemoveElement:
-                    await this.removeElementFromGraph(event);
+                await this.editorRemoveElement(event);
                 break;
         }
-        this._runningProcess = false;
+
+        this.inProgress = false;
     }
 
     public static setEditorMode(selectedMode: EditorMode): void {
-        this._editorSelectedMode = selectedMode;
+        this.selectedEditorMode = selectedMode;
     }
 
     public static getProcessStatus(): boolean {
-        return this._runningProcess;
+        return this.inProgress;
     }
 
     public static setProcessStatus(state: boolean): void {
-        this._runningProcess = state;
+        this.inProgress = state;
     }
 
-    public static getNodesList(): GraphNode[] {
-        return this._graphNodesList;
+    public static getVertices(): Vertex[] {
+        return this.vertices;
     }
 
-    public static resetNodesListColour(): void {
-        if (this._runningProcess == false) {
-            this._graphNodesList.forEach(node => {
-                node.setNodeColour("white");
-                node.setTextColour("black");
+    public static resetVertices(): void {
+        if (this.inProgress == false) {
+            this.vertices.forEach(v => {
+                v.setCircleColour("white");
+                v.setTextColour("black");
             });
         }
     }
 
     public static editorClearAll(): void {
-        if (this._runningProcess == true) return;
+        if (this.inProgress == true) return;
 
-        if (this._awaitingEndNode == true) {
-            this._startNode!.toggleSelected();
-            this._startNode = null;
-            this._awaitingEndNode = false;
+        if (this.drawingEdge == true) {
+            this.selectedVertex!.toggleSelected();
+            this.selectedVertex = null;
+            this.drawingEdge = false;
         }
 
-        if (confirm("Are you sure you want to clear all nodes and edges?") == true) {
-            document.getElementById("graphEditorEdgesGroup")!.replaceChildren();
-            document.getElementById("graphEditorNodesGroup")!.replaceChildren();
-            this._graphNodesList = new Array();
-            this._currentNodeValues = new Array();
+        if (confirm("Are you sure you want to clear all vertices and edges?") == true) {
+            document.getElementById("graphEdgesGroup")!.replaceChildren();
+            document.getElementById("graphVerticesGroup")!.replaceChildren();
+            this.vertices = new Array();
+            this.reservedValues = new Array();
         }
     }
 }
@@ -201,10 +212,10 @@ abstract class GraphAlgorithmVisualiser {
 
         switch (this._graphSelectedAlgorithm) {
             case AlgorithmSelection.BFS:
-                await BFS.runAlgorithm(GraphEditor.getNodesList());
+                await BFS.runAlgorithm(GraphEditor.getVertices());
                 break;
             case AlgorithmSelection.DFS:
-                await DFS.runAlgorithm(GraphEditor.getNodesList());
+                await DFS.runAlgorithm(GraphEditor.getVertices());
                 break;
         }
 
@@ -216,27 +227,47 @@ abstract class GraphAlgorithmVisualiser {
     }
 }
 
-document.getElementById("graphEditorWindow")!.addEventListener("click", (event) => {
-    GraphEditor.editorInteractionEvent(event);
-}, false);
+document.getElementById("editorWindow")!.addEventListener(
+    "click",
+    event => {
+        GraphEditor.editorEvent(event);
+    },
+    false
+);
 
-document.getElementById("graphEditorModeSelect")!.addEventListener("input", (event) => {
-    GraphEditor.setEditorMode((event.target as HTMLInputElement).value as EditorMode);
-}, false);
+document.getElementById("editorModeSelect")!.addEventListener(
+    "input",
+    event => {
+        GraphEditor.setEditorMode((event.target as HTMLInputElement).value as EditorMode);
+    },
+    false
+);
 
-document.getElementById("graphEditorClearAll")!.addEventListener("click", () => {
-    GraphEditor.editorClearAll();
-}, false);
+document.getElementById("editorClearAll")!.addEventListener(
+    "click",
+    () => {
+        GraphEditor.editorClearAll();
+    },
+    false
+);
 
-document.getElementById("graphAlgorithmSelect")!.addEventListener("input", (event) => {
-    GraphAlgorithmVisualiser.setAlgorithm((event.target as HTMLInputElement).value as AlgorithmSelection);
-}, false);
+document.getElementById("algorithmSelect")!.addEventListener(
+    "input",
+    event => {
+        GraphAlgorithmVisualiser.setAlgorithm((event.target as HTMLInputElement).value as AlgorithmSelection);
+    },
+    false
+);
 
-document.getElementById("graphAlgorithmRunButton")!.addEventListener("click", () => {
-    GraphEditor.resetNodesListColour();
-    GraphAlgorithmVisualiser.runAlgorithm();
-}, false)
+document.getElementById("runAlgorithm")!.addEventListener(
+    "click",
+    () => {
+        GraphEditor.resetVertices();
+        GraphAlgorithmVisualiser.runAlgorithm();
+    },
+    false
+);
 
 document.getElementById("graphReset")!.addEventListener("click", () => {
-    GraphEditor.resetNodesListColour();
-})
+    GraphEditor.resetVertices();
+});
