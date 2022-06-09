4abdd248efe7d30f0f9c3f64ce7284d34273115b
Dakskihedron <47210659+Dakskihedron@users.noreply.github.com>
2022-06-10 02:29:00 +1200

    Add stack and minor changes

--- src/utils.ts
+++ src/utils.ts
@@ -5,29 +5,57 @@ export async function wait(ms: number = 1000): Promise<void> {
 }
 
 export class Queue<T> {
-    private _data: T[] = new Array();
+    private data: T[] = new Array();
 
     public enqueue(item: T): void {
-        this._data.push(item);
+        this.data.push(item);
     }
 
-    public dequeue(): void | undefined {
-        this._data.splice(0, 1);
+    public dequeue(): void {
+        this.data.splice(0, 1);
     }
 
     public peek(): T | undefined {
-        return this._data[0];
+        return this.data[0];
     }
 
     public size(): number {
-        return this._data.length;
+        return this.data.length;
     }
 
     public isEmpty(): boolean {
-        return this._data.length == 0;
+        return this.data.length == 0;
     }
 
     public toString(): string {
-        return `<- |${this._data.join(", ")}| <-`;
+        return `<- |${this.data.join(", ")}| <-`;
+    }
+}
+
+export class Stack<T> {
+    private data: T[] = new Array();
+
+    public push(item: T): void {
+        this.data.push(item);
+    }
+
+    public pop(): void {
+        this.data.pop();
+    }
+
+    public peek(): T {
+        return this.data[this.data.length - 1];
+    }
+
+    public size(): number {
+        return this.data.length;
+    }
+
+    public isEmpty(): boolean {
+        return this.data.length == 0;
+    }
+
+    public toString(): string {
+        return `[${this.data.join(", ")} <-`;
     }
 }
